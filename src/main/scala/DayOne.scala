d534f0cfd684bd5cbc33fb5a2b4ad2c3e15c56c2
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-12-01 14:36:22 +0000

    Day One: Correct answer achieved

--- src/main/scala/DayOne.scala
+++ src/main/scala/DayOne.scala
@@ -3,12 +3,9 @@ import scala.io.Source
 case class DayOne() {
 
   def calibrationValuesSum(filename: String): Int = {
-    val calibrationLines = getLinesFromFile(filename)
-
-    calibrationLines.foreach(println(_))
-
-    val sum = 142
-    sum
+    val calibrationLines: Seq[String] = getLinesFromFile(filename)
+    val valuesPerLine: Seq[Int] = calibrationLines.map(valuePerLine)
+    valuesPerLine.sum
   }
 
   private def getLinesFromFile(filename: String): List[String] = {
@@ -18,4 +15,16 @@ case class DayOne() {
     lines
   }
 
+  private def valuePerLine(line: String): Int = {
+    val allIntegers: Seq[Char] = line.filter(_.isDigit)
+    s"${allIntegers.head}${allIntegers.last}".toInt
+  }
+
+}
+
+object DayOneTask {
+  def main(args: Array[String]): Unit = {
+    val sum = DayOne().calibrationValuesSum("day-one-input.txt")
+    println(sum)
+  }
 }
