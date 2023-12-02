cc138b694c3b265ca36642abae1de93e65f7608c
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-12-02 17:38:04 +0000

    Day Two: Refactored task one

--- src/main/scala/DayTwo.scala
+++ src/main/scala/DayTwo.scala
@@ -10,21 +10,23 @@ case class DayTwo() {
 
   def calculate(filename: String): Int = {
     val lines = Source.fromResource(filename).getLines().toList
-    val linesWithIndex = Source.fromResource(filename).getLines().toList.zip(1 to lines.length)
-    linesWithIndex.map(lineWithIndex =>
-      checkIfPossible(lineWithIndex._2, lineWithIndex._1)
-    ).filter(!_._2).map(_._1).sum
+    lines.map(possibleGameNumbers).sum
   }
 
-  private def checkIfPossible(gameNumber: Int, line: String): (Int, Boolean) = {
+  private def possibleGameNumbers(line: String): Int = {
+    val gameNumberAndSets = line.split(":").toList
+
+    val gameNumber =
+      gameNumberAndSets.head.split(" ").last.toInt
+
     val numberOfCubesAndColour =
-      parseLineToNumbersAndColour(line.split(":").toList.last)
+      parseLineToNumbersAndColour(gameNumberAndSets.last)
 
     val impossibles = numberOfCubesAndColour.filter  { gameSet =>
       gameSet._1 > maxCubesPerColour(gameSet._2)
     }
 
-    (gameNumber, impossibles.nonEmpty)
+    if (impossibles.isEmpty) gameNumber else 0
   }
 
   private def parseLineToNumbersAndColour(line: String): List[(Int, String)] = {
