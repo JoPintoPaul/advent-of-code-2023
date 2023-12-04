4227939eb8463a5b8a10dd08bb99ae6f886bd5e8
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-12-04 13:26:19 +0000

    Day Four: Correct solution for part 1

--- src/main/scala/DayFour.scala
+++ src/main/scala/DayFour.scala
@@ -4,31 +4,33 @@ case class DayFour() {
 
   def calculate(filename: String) = {
     val cards: Seq[String] = Source.fromResource(filename).getLines().toList
-    val totalPointsValue: Int = calculateTotalPointsValue(cards)
-    totalPointsValue
+    calculateTotalPointsValue(cards)
   }
-
 
   private def calculateTotalPointsValue(cards: Seq[String]): Int = {
-    val parsedWinnersAndChosen: Seq[(Seq[Int], Seq[Int])] =
-      cards map { card =>
-        val winnersAndChosen: (Seq[Int], Seq[Int]) = parseCard(card)
-        winnersAndChosen
-      }
-
-    val numberOfMatchesPerCard: Seq[Int] = parsedWinnersAndChosen map { card =>
-      val numberOfMatches: Int = matchWinnersAndChosen(card)
-      numberOfMatches
-    }
-
-    numberOfMatchesPerCard.map(pointsForMatches).sum
+    cards.map { card =>
+      pointsForMatches(numberOfMatches(parseWinnersAndChoices(card)))
+    }.sum
   }
 
-  private def parseCard(card: String): (Seq[Int], Seq[Int]) = ???
+  private def parseWinnersAndChoices(card: String): (Seq[Int], Seq[Int]) = {
+    val winnersAndChoices = card.split(""":""").toList.last.trim.split('|').toList
+    (toListInts(winnersAndChoices.head), toListInts(winnersAndChoices.last))
+  }
 
-  private def matchWinnersAndChosen(card: (Seq[Int], Seq[Int])): Int = ???
+  private def toListInts(ints: String) =
+    ints.trim.split(" ").toList.filter(_.nonEmpty).map(_.toInt)
+
+  private def numberOfMatches(winnersAndChoices: (Seq[Int], Seq[Int])): Int =
+    winnersAndChoices._1.intersect(winnersAndChoices._2).length
 
   private def pointsForMatches(matches: Int): Int = {
     if (matches == 0) 0 else Math.pow(2, matches -1).toInt
+  }
+}
+
+object DayFour {
+  def main(args: Array[String]): Unit = {
+    println(DayFour().calculate("day-four-input.txt"))
   }
 }
