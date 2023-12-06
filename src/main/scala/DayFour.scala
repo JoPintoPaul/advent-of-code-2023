e2125880fd8e73912c10b786ed8eed17dad74493
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2023-12-06 09:32:16 +0000

    Days Three and Four: Removed incomplete Day Three, refactored Day 4

--- src/main/scala/DayFour.scala
+++ src/main/scala/DayFour.scala
@@ -1,12 +1,9 @@
 import scala.annotation.tailrec
-import scala.collection.immutable.ListMap
 import scala.io.Source
 
 case class DayFour(filename: String) {
 
   private val lines = Source.fromResource(filename).getLines().toList
-
-  case class Card(gameNumber: Int, winningMatches: Int)
 
   def calculatePoints(): Int = {
     @tailrec
@@ -15,7 +12,7 @@ case class DayFour(filename: String) {
       else {
         doPoints(
           unprocessedLines.tail,
-          currentTotal + pointsForMatches(numberOfMatches(parseWinnersAndChoices(unprocessedLines.head)))
+          currentTotal + pointsForMatches(getWinningMatchCount(parse(unprocessedLines.head)))
         )
       }
     }
@@ -24,42 +21,38 @@ case class DayFour(filename: String) {
   }
 
   def calculateCopies(): Int = {
-
-    val upperBound = lines.length
-
+    @tailrec
     def doCopies(gameNumber: Int, unprocessedLines: Seq[String], copies: Map[Int, Int]): Map[Int, Int] = {
-
       @tailrec
-      def updateCopies(copiesToAdd: Seq[Int], ticketCount: Map[Int, Int]): Map[Int, Int] = {
-        val updatedTicketCount =
-          if (ticketCount.contains(gameNumber)) ticketCount else ticketCount + (gameNumber -> 1)
-
-        if (copiesToAdd.isEmpty) updatedTicketCount
+      def updateCopies(copiesToAdd: Seq[Int], previousTicketCount: Map[Int, Int]): Map[Int, Int] = {
+        val ticketCountWithCurrentGame =
+          if (previousTicketCount.contains(gameNumber)) previousTicketCount else previousTicketCount + (gameNumber -> 1)
+        if (copiesToAdd.isEmpty) ticketCountWithCurrentGame
         else {
-          val numberToAdd: Int = updatedTicketCount(gameNumber)
-          val updatedMap =
-            updatedTicketCount + (copiesToAdd.head -> (updatedTicketCount.getOrElse(copiesToAdd.head, 1) + numberToAdd))
-          updateCopies(copiesToAdd.tail, updatedMap)
+          val updatedTicketCount =
+            ticketCountWithCurrentGame +
+              (copiesToAdd.head ->
+                (ticketCountWithCurrentGame.getOrElse(copiesToAdd.head, 1) + ticketCountWithCurrentGame(gameNumber))
+                )
+
+          updateCopies(copiesToAdd.tail, updatedTicketCount)
         }
       }
 
       if (unprocessedLines.isEmpty) copies
       else {
-        val numberOfWinningMatches: Int = numberOfMatches(parseWinnersAndChoices(unprocessedLines.head))
-        val copiesToMake: Seq[Int] =
-          if (numberOfWinningMatches == 0) Nil
-          else (1 to numberOfWinningMatches) map {_ + gameNumber} filterNot(_ > upperBound)
-        val updatedMap = updateCopies(copiesToMake, copies)
-
-        doCopies(gameNumber + 1, unprocessedLines.tail, updatedMap)
+        val winningMatchCount: Int = getWinningMatchCount(parse(unprocessedLines.head))
+        val gamesToCopy: Seq[Int] =
+          if (winningMatchCount == 0) Nil
+          else (1 to winningMatchCount) map {_ + gameNumber} filterNot(_ > lines.length)
+        doCopies(gameNumber + 1, unprocessedLines.tail, updateCopies(gamesToCopy, copies))
       }
     }
 
-    val result = doCopies(1, lines, Map.empty)
-    result.values.sum
+    doCopies(1, lines, Map.empty).values.sum
   }
 
-  private def parseWinnersAndChoices(card: String): (Seq[Int], Seq[Int]) = {
+  private def parse(card: String): (Seq[Int], Seq[Int]) = {
     val winnersAndChoices = card.split(""":""").toList.last.trim.split('|').toList
     (toListInts(winnersAndChoices.head), toListInts(winnersAndChoices.last))
   }
@@ -67,7 +60,7 @@ case class DayFour(filename: String) {
   private def toListInts(ints: String) =
     ints.trim.split(" ").toList.filter(_.nonEmpty).map(_.toInt)
 
-  private def numberOfMatches(winnersAndChoices: (Seq[Int], Seq[Int])): Int =
+  private def getWinningMatchCount(winnersAndChoices: (Seq[Int], Seq[Int])): Int =
     winnersAndChoices._1.intersect(winnersAndChoices._2).length
 
   private def pointsForMatches(matches: Int): Int = {
@@ -77,9 +70,9 @@ case class DayFour(filename: String) {
 
 object DayFour {
   def main(args: Array[String]): Unit = {
-    val cardsChallenge = DayFour("day-four-input.txt")
-    println(s"Day Four, Task One: ${cardsChallenge.calculatePoints()}")
-    println(s"Day Four, Task Two: ${cardsChallenge.calculateCopies()}")
+    val challenge = DayFour("day-four-input.txt")
+    println(s"Day Four, Task One: ${challenge.calculatePoints()}")
+    println(s"Day Four, Task Two: ${challenge.calculateCopies()}")
 
   }
 }
